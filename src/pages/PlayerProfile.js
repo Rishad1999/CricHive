d304d4b0cd5f73ac9d9461fd3123225ef45e5451
RishadM <rishmohamed1999@gmail.com>
2023-07-11 11:27:37 +0530

    default and searched page

--- src/pages/PlayerProfile.js
+++ src/pages/PlayerProfile.js
@@ -1,63 +1,99 @@
+import React, { useState } from "react";
 import PlayerImg from "../assets/dhoni.png";
 import { TableStat, Footer, SearchPlayer } from "../components";
 import "tailwindcss/tailwind.css";
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+
+// Initialize Firebase
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export const PlayerProfile = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [playerDetails, setPlayerDetails] = useState({});
+
+  const handleSearch = async (query) => {
+    setSearchQuery(query);
+
+    try {
+      const response = await firebase
+        .database()
+        .ref("Mahendra Singh Dhoni")
+        .orderByChild("FULL NAME")
+        .equalTo(query)
+        .once("value");
+      const data = response.val();
+
+      if (data) {
+        const playerData = Object.values(data)[0];
+        setPlayerDetails(playerData);
+      } else {
+        setPlayerDetails({});
+      }
+    } catch (error) {
+      console.log(error);
+      setPlayerDetails({});
+    }
+  };
+
   return (
     <>
       <div className="mx-20 mt-5 rounded-lg">
-        <SearchPlayer />
+        <SearchPlayer onSearch={handleSearch} />
       </div>
 
-      <div class="shadow-2xl mx-20 mt-5 rounded-lg h-32 sm:h-48 md:h-64 lg:h-96 bg-gradient-to-r from-gray-500 to-slate-300 relative ...">
+      <div className="shadow-2xl mx-20 mt-5 rounded-lg h-32 sm:h-48 md:h-64 lg:h-96 bg-gradient-to-r from-gray-500 to-slate-300 relative ...">
         <div className="absolute bottom-0 left-0 ml-10 mb-10">
-          <h1 className="text-white text-4xl">M.S. Dhoni</h1>
+          <h1 className="text-white text-4xl">{playerDetails["FULL NAME"]}</h1>
           <p className="text-white text-3xl">
             India <span>| Wicket Keeper</span>
           </p>
         </div>
-        <div class="w-60 absolute bottom-0 right-10 ...">
+        <div className="w-60 absolute bottom-0 right-10 ...">
           <img src={PlayerImg} alt="Logo" />
         </div>
       </div>
 
       <div className="bg-gradient-to-r from-gray-500 to-slate-300 rounded-lg mx-20 mt-5 shadow-xl">
-        <div class="grid grid-cols-3 gap-4 font-medium">
+        <div className="grid grid-cols-3 gap-4 font-medium">
           <div className="p-4 bg-gradient-to-r from-slate-50 to-slate-50 m-3 rounded-lg">
             <h2>FULL NAME</h2>
-            <p>Mahendra Singh Dhoni</p>
+            <p>{playerDetails["FULL NAME"]}</p>
           </div>
           <div className="p-4 bg-gradient-to-r from-slate-50 to-slate-50 m-3 rounded-lg">
             <h2>BORN</h2>
-            <p>July 07, 1981, Ranchi, Bihar</p>
+            <p>{playerDetails["BORN"]}</p>
           </div>
           <div className="p-4  bg-gradient-to-r from-slate-50 to-slate-50 m-3 rounded-lg">
             <h2>AGE</h2>
-            <p>41y 240d</p>
+            <p>{playerDetails["AGE"]}</p>
           </div>
           <div className="p-4  bg-gradient-to-r from-slate-50 to-slate-50 m-3 rounded-lg">
             <h2>HIGHEST RUNS</h2>
-            <p>102*</p>
+            <p>{playerDetails["HIGHEST RUNS"]}</p>
           </div>
           <div className="p-4  bg-gradient-to-r from-slate-50 to-slate-50 m-3 rounded-lg">
             <h2>BATTING STYLE</h2>
-            <p>Right hand Bat</p>
+            <p>{playerDetails["BATTING STYLE"]}</p>
           </div>
           <div className="p-4  bg-gradient-to-r from-slate-50 to-slate-50 m-3 rounded-lg">
             <h2>BOWLING STYLE</h2>
-            <p>Right arm Medium</p>
+            <p>{playerDetails["BOWLING STYLE"]}</p>
           </div>
           <div className="p-4  bg-gradient-to-r from-slate-50 to-slate-50 m-3 rounded-lg">
             <h2>FIELDING POSITION</h2>
-            <p>Wicketkeeper</p>
+            <p>{playerDetails["FIELDING POSITION"]}</p>
           </div>
           <div className="p-4  bg-gradient-to-r from-slate-50 to-slate-50 m-3 rounded-lg">
             <h2>PLAYING ROLE</h2>
-            <p>Wicketkeeper Batter</p>
+            <p>{playerDetails["PLAYING ROLE"]}</p>
           </div>
           <div className="p-4  bg-gradient-to-r from-slate-50 to-slate-50 m-3 rounded-lg">
             <h2>ALSO KNOWN AS</h2>
-            <p>Mahi</p>
+            <p>{playerDetails["ALSO KNOWN AS"]}</p>
           </div>
         </div>
       </div>
@@ -98,72 +134,10 @@ export const PlayerProfile = () => {
                 <th scope="col" className="px-6 py-3 text-right">
                   Economy
                 </th>
-                {/* <th scope="col" className="px-6 py-3">
-            <span className="sr-only">Edit</span>
-          </th> */}
               </tr>
             </thead>
             <tbody>
-              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  2019
-                </th>
-                <td className="px-6 py-4">6</td>
-                <td className="px-6 py-4">13</td>
-                <td className="px-6 py-4">240</td>
-                <td className="px-6 py-4 text-right">6.8</td>
-                {/* <td className="px-6 py-4 text-right">
-            <a
-              href="#"
-              className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-            >
-              Edit
-            </a>
-          </td> */}
-              </tr>
-              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  2018
-                </th>
-                <td className="px-6 py-4">5</td>
-                <td className="px-6 py-4">20</td>
-                <td className="px-6 py-4">100</td>
-                <td className="px-6 py-4 text-right">4.7</td>
-                {/* <td className="px-6 py-4 text-right">
-            <a
-              href="#"
-              className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-            >
-              Edit
-            </a>
-          </td> */}
-              </tr>
-              <tr className="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  2017
-                </th>
-                <td className="px-6 py-4">9</td>
-                <td className="px-6 py-4">30</td>
-                <td className="px-6 py-4">560</td>
-                <td className="px-6 py-4 text-right">5.4</td>
-                {/* <td className="px-6 py-4 text-right">
-            <a
-              href="#"
-              className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-            >
-              Edit
-            </a>
-          </td> */}
-              </tr>
+              {/* Table rows */}
             </tbody>
           </table>
         </div>
