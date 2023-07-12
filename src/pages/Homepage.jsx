a0117a9f1ed3f24289e7aa566c02d84bb89b7f60
ssadeepa <sumudithsadee@gmail.com>
2023-07-12 20:03:00 +0530

    homepage.js add posts

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -16,10 +16,10 @@ export const Homepage = () => {
   const postsRef = collection(db, "player_profile");
 
   useEffect(() => {
-    async function getPosts(){
+    async function getPosts() {
       const data = await getDocs(postsRef);
-      setPosts(data.docs.map((document) => (
-        {...document.data(), id: document.id}))
+      setPosts(
+        data.docs.map((document) => ({ ...document.data(), id: document.id }))
       );
     }
     console.log("---");
@@ -38,8 +38,8 @@ export const Homepage = () => {
         <p className="text-center font-medium">MATCH STORIES</p>
       </div>
 
-      { posts.map(post => (
-        <div class=" flex flex-wrap justify-center space-x-8 mx-auto max-w-screen-xl">
+      {/* { posts.map(post => ())} */}
+      <div class=" flex flex-wrap justify-center space-x-8 mx-auto max-w-screen-xl">
         <div class="basis-1/2 p-8">
           <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <Link to="#">
@@ -48,16 +48,26 @@ export const Homepage = () => {
             <div className="p-5">
               <Link to="#">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {post.name}
+                  Williamson not giving up on the World Cup dream just yet
                 </h5>
               </Link>
               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                {post.population}
+                Kane Williamson has not given up on playing a part in New
+                Zealand's 2023 ODI World Cup campaign as he takes his recovery
+                from a right knee injury on a "week-to-week" basis. Williamson,
+                who ruptured his ACL while fielding for Gujarat Titans against
+                Chennai Super Kings in the opening game of IPL 2023 on March 31
+                and underwent surgery in early April, said ensuring the knee
+                mended properly was his overriding priority. "Very much just
+                trying to keep it week-to-week at the moment," Williamson said
+                on Monday. "I haven't had such a long-term injury before but
+                talking to other people that have, the journey is a bit of a
+                long one so if you look too far ahead.
               </p>
             </div>
           </div>
         </div>
-        {/* <div class="basis-1/2 p-8">
+        <div class="basis-1/2 p-8">
           <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <Link to="#">
               <img className="rounded-t-lg" src={post_waniya} alt="" />
@@ -79,13 +89,12 @@ export const Homepage = () => {
                 economy rate of 7.68 also showed that he played a good
                 supporting role to the impressive Netherlands pace attack. The
                 Player of the Tournament, Sam Curran finished with 13 wickets,
-                five of them coming in a single game where he registered 
+                five of them coming in a single game where he registered
               </p>
             </div>
           </div>
-        </div> */}
+        </div>
       </div>
-      ))}
 
       <Footer />
     </>
